41dc1ac3533a94871a56a06cb7614f5719b4b699
Taisuke Fukuno <fukuno@TPC0129.local>
2018-04-10 13:54:36 +0900

    fixed

--- serviceworker.js
+++ serviceworker.js
@@ -1,27 +1,36 @@
 var VERSION = 1;
 var STATIC_CACHE_NAME = 'static_' + VERSION;
 
-var ORIGIN = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+var ORIGIN = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + "/progrun/";
 
-var STATIC_FILES = [
-	ORIGIN + '/born/',
-	ORIGIN + '/born/fukuno.js',
-	ORIGIN + '/born/data-born.json'
+var FILES = [
+	"progrun.png",
+	"progrun-192x192.png",
+	"base.css",
+	"progrun.css",
+	"util.js",
+	"https://fonts.googleapis.com/css?family=Sansita+One",
+	"https://fonts.googleapis.com/css?family=Source+Code+Pro:600,900",
 ];
-for (var i = 1; i <= 25; i++) {
-	STATIC_FILES.push("https://codeforfukui.github.io/born/images/" + i + ".jpg");
-}
+var STATIC_FILES = [];
+FILES.forEach(function(s) {
+	if (!(s.startsWith("http://") || s.startsWith("https://"))) {
+		s = ORIGIN + s;
+	}
+	STATIC_FILES.push(s);
+});
+
 var STATIC_FILE_URL_HASH = {};
 STATIC_FILES.forEach(function(x) { STATIC_FILE_URL_HASH[x] = true; });
 
 self.addEventListener('install', function(evt) {
 	evt.waitUntil(caches.open(STATIC_CACHE_NAME).then(function(cache) {
 		return Promise.all(STATIC_FILES.map(function(url) {
-//			console.log(url);
+			console.log(url);
 			return fetch(new Request(url)).then(function(response) {
 				if (response.ok)
 					return cache.put(response.url, response);
-				return Promise.reject("Invalid res " + response.url + " " + response.status);
+				return Promise.reject("Invalid response " + response.url + " " + response.status);
 			});
 		}));
 	}));
